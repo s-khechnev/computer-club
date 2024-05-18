4be19cdaefa474ad40bd5e993039c7a7eb1baca0
Semyon Khechnev <hechnev@gmail.com>
2024-05-19 02:35:19 +0300

    Fix events
    Remove useless BaseWithName class, define << to ostream

--- include/Event.hpp
+++ include/Event.hpp
@@ -2,6 +2,7 @@
 #define EVENT_HPP_
 
 #include <chrono>
+#include <iostream>
 #include <string>
 
 namespace event {
@@ -11,68 +12,62 @@ using time_point =
 
 enum class Id {
   Come = 1,
-  Seat = 2,
-  Wait = 3,
-  Left = 4,
+  Seat,
+  Wait,
+  Left,
 };
+
+std::ostream& operator<<(std::ostream& os, Id id) {
+  os << static_cast<int>(id);
+  return os;
+}
 
 class Base {
  public:
   time_point getTime() const { return time; }
   Id getId() const { return id; }
+  const std::string& getName() const { return name; }
+
   virtual ~Base() = default;
 
  protected:
-  Base(time_point time, Id id) : time(time), id(id) {}
+  Base(time_point time, Id id, const std::string& name)
+      : time(time), id(id), name(name) {}
 
  private:
   const time_point time;
   const Id id;
-};
-
-class BaseWithName : public Base {
- public:
-  const std::string& getName() const { return name; }
-
- protected:
-  BaseWithName(time_point time, Id id, const std::string& name)
-      : Base(time, id), name(name) {}
-
- private:
   const std::string name;
 };
 
-class BaseWithNameTableNum : public BaseWithName {
+class BaseWithTableNum : public Base {
  public:
   unsigned getTableNum() const { return tableNum; }
 
  protected:
-  BaseWithNameTableNum(time_point time, Id id, const std::string& name,
-                       unsigned tableNum)
-      : BaseWithName(time, id, name), tableNum(tableNum) {}
+  BaseWithTableNum(time_point time, Id id, const std::string& name,
+                   unsigned tableNum)
+      : Base(time, id, name), tableNum(tableNum) {}
 
  private:
   const unsigned tableNum;
 };
 
-struct Come final : public BaseWithName {
-  Come(time_point time, const std::string& name)
-      : BaseWithName(time, Id::Come, name) {}
+struct Come final : public Base {
+  Come(time_point time, const std::string& name) : Base(time, Id::Come, name) {}
 };
 
-struct Seat final : public BaseWithNameTableNum {
+struct Seat final : public BaseWithTableNum {
   Seat(time_point time, const std::string& name, unsigned tableNum)
-      : BaseWithNameTableNum(time, Id::Seat, name, tableNum) {}
+      : BaseWithTableNum(time, Id::Seat, name, tableNum) {}
 };
 
-struct Wait final : public BaseWithName {
-  Wait(time_point time, const std::string& name)
-      : BaseWithName(time, Id::Wait, name) {}
+struct Wait final : public Base {
+  Wait(time_point time, const std::string& name) : Base(time, Id::Wait, name) {}
 };
 
-struct Left final : public BaseWithName {
-  Left(time_point time, const std::string& name)
-      : BaseWithName(time, Id::Left, name) {}
+struct Left final : public Base {
+  Left(time_point time, const std::string& name) : Base(time, Id::Left, name) {}
 };
 
 }  // namespace event
