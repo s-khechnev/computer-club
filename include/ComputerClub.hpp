822600512cefe526331d78162368350cf75ca987
Semyon Khechnev <hechnev@gmail.com>
2024-05-18 22:09:49 +0300

    Add sketch of solution

--- include/ComputerClub.hpp
+++ include/ComputerClub.hpp
@@ -2,11 +2,11 @@
 #define COMPUTERCLUB_HPP_
 
 #include <chrono>
+#include <iostream>
 #include <list>
-#include <unordered_set>
+#include <unordered_map>
 #include <vector>
 
-#include "Client.hpp"
 #include "Table.hpp"
 
 class ComputerClub {
@@ -18,50 +18,93 @@ class ComputerClub {
 
   ComputerClub(unsigned tableNum, time_point openTime, time_point closeTime,
                unsigned pricePerHour)
-      : tableNum(tableNum),
-        openTime(openTime),
+      : openTime(openTime),
         closeTime(closeTime),
-        pricePerHour(pricePerHour),
         tables(tableNum + 1, Table{pricePerHour}),
         freeTables(tableNum) {}
 
-  unsigned getTableNum() const { return tableNum; }
+  unsigned getTableNum() const { return tables.size(); }
   time_point getOpenTime() const { return openTime; }
   time_point getCloseTime() const { return closeTime; }
-  unsigned getPricePerHour() const { return pricePerHour; }
+  unsigned getFreeTables() const { return freeTables; }
+  std::size_t getQueueSize() const { return queue.size(); }
 
   bool isTableBusy(unsigned tableN) const { return tables[tableN].busy(); }
-  std::size_t sizeQueue() const { return queue.size(); }
 
-  bool containsClient(const Client& client) const {
+  void addClient(const std::string& client) { clients[client] = 0; }
+
+  bool containsClient(const std::string& client) const {
     return clients.contains(client);
   }
 
-  void addClient(const Client& client) { clients.insert(client); }
+  bool isClientPlaying(const std::string& client) const {
+    return (clients.find(client)->second) != 0;
+  }
 
-  void occupyTable(Client& client, unsigned tableN, time_point time) {
+  void removeClientAndInviteWaiting(const std::string& client,
+                                    time_point time) {
+    if (!isClientPlaying(client)) {
+      // fix me
+      queue.remove(client);
+      return;
+    }
+
+    unsigned tableN = clients.find(client)->second;
+    releaseTable(client, tableN, time);
+    clients.erase(client);
+
+    if (getQueueSize() == 0) return;
+
+    std::string waiting = queue.front();
+    occupyTable(waiting, tableN, time);
+    queue.pop_front();
+  }
+
+  void clientGoWait(const std::string& client, time_point time) {
+    if (isClientPlaying(client))
+      releaseTable(client, clients.find(client)->second, time);
+    enqueue(clients.find(client)->first);
+  }
+
+  void occupyTable(const std::string& client, unsigned tableN,
+                   time_point time) {
+    if (isClientPlaying(client))
+      releaseTable(client, clients.find(client)->second, time);
+
     tables[tableN].occupy(time);
-    client.curTable = tableN;
+    auto clientIt = clients.find(client);
+    clientIt->second = tableN;
     --freeTables;
   }
 
-  void releaseTable(Client& client, unsigned tableN, time_point time) {
+  void releaseTable(const std::string& client, unsigned tableN,
+                    time_point time) {
     tables[tableN].release(time);
-    client.curTable = 0;
+    clients[client] = 0;
     ++freeTables;
   }
 
-  void enqueue(Client& client) { queue.push_back(client); }
-  Client& dequeue() { return queue.front(); }
-  void pop() { return queue.pop_front(); }
+  void enqueue(const std::string& client) { queue.push_back(client); }
+
+  void printStat() {
+    int i = 1;
+    for (auto it = ++tables.begin(), end = tables.end(); it != end; ++it, ++i) {
+      std::cout << i << " " << it->getProfit() << " "
+                << it->getTotalUsage().count() << "\n";
+    }
+  }
+
+  void close() {
+    for (auto& table : tables) {
+      if (table.busy()) table.release(closeTime);
+    }
+  }
 
  private:
-  unsigned tableNum = 0;
   time_point openTime, closeTime;
-  unsigned pricePerHour = 0;
 
-  std::unordered_set<Client> clients;
-  std::list<Client> queue;
+  std::unordered_map<std::string, unsigned> clients;
+  std::list<std::string> queue;
   std::vector<Table> tables;
   unsigned freeTables;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,106 @@
+#include <chrono>
+#include <ctime>
 #include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "ComputerClub.hpp"
+#include "Event.hpp"
+#include "Parser.hpp"
+
+void printTime(event::time_point time) {
+  using namespace std::chrono;
+  std::time_t timeT = system_clock::to_time_t(time);
+  std::tm localTime = *std::gmtime(&timeT);
+  std::cout << std::put_time(&localTime, "%H:%M") << "\n";
+}
+
+void printError(event::time_point time, std::string error) {
+  using namespace std::chrono;
+  std::time_t timeT = system_clock::to_time_t(time);
+  std::tm localTime = *std::gmtime(&timeT);
+  std::cout << std::put_time(&localTime, "%H:%M") << " " << 13 << " " << error
+            << "\n";
+}
 
 int main() {
-  std::cout << "Hello world!" << std::endl;
+  ComputerClub club;
+  std::vector<std::unique_ptr<event::Base>> events;
+  try {
+    std::tie(club, events) = parser::parse("123.txt");
+  } catch (const std::runtime_error& err) {
+    std::cout << err.what() << "\n";
+  }
+
+  printTime(club.getOpenTime());
+
+  for (auto& e : events) {
+    switch (e->getId()) {
+      case event::Id::Come: {
+        auto* event = static_cast<event::Come*>(e.get());
+        if (club.containsClient(event->getName())) {
+          printError(event->getTime(), "YouShallNotPass");
+          continue;
+        }
+        if (event->getTime() < club.getOpenTime()) {
+          printError(event->getTime(), "NotOpenYet");
+          continue;
+        }
+
+        club.addClient(event->getName());
+        continue;
+      }
+      case event::Id::Seat: {
+        auto* event = static_cast<event::Seat*>(e.get());
+        if (club.isTableBusy(event->getTableNum())) {
+          printError(event->getTime(), "PlaceIsBusy");
+          continue;
+        }
+        if (!club.containsClient(event->getName())) {
+          printError(event->getTime(), "ClientUnknown");
+          continue;
+        }
+
+        club.occupyTable(event->getName(), event->getTableNum(),
+                         event->getTime());
+        continue;
+      }
+      case event::Id::Wait: {
+        auto* event = static_cast<event::Wait*>(e.get());
+        if (club.getFreeTables() > 0) {
+          printError(event->getTime(), "ICanWaitNoLonger!");
+          continue;
+        }
+        if (club.getQueueSize() > club.getTableNum()) {
+          std::cout << "I go home 11 \n";
+          continue;
+        }
+        club.clientGoWait(event->getName(), event->getTime());
+        continue;
+      }
+      case event::Id::Left: {
+        auto* event = static_cast<event::Left*>(e.get());
+        if (!club.containsClient(event->getName())) {
+          printError(event->getTime(), "ClientUnknown");
+          continue;
+        }
+
+        // split to two funcs
+        club.removeClientAndInviteWaiting(event->getName(), event->getTime());
+        // 12
+
+        continue;
+      }
+    }
+  }
+
+  club.close();
+  club.printStat();
+
+  printTime(club.getCloseTime());
 
   return 0;
 }
