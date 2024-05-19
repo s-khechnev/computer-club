ef3c1009cc02923dacc89214259d255939802182
Semyon Khechnev <hechnev@gmail.com>
2024-05-19 06:06:04 +0300

    Refactor parser: add ranges, naming
    decrease allocs 91 -> 47

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -2,8 +2,11 @@
 #define PARSER_HPP_
 
 #include <chrono>
+#include <exception>
 #include <fstream>
 #include <iomanip>
+#include <ranges>
+#include <stdexcept>
 #include <vector>
 
 #include "ComputerClub.hpp"
@@ -14,42 +17,28 @@ namespace parser {
 unsigned stoui(const std::string& str) {
   std::size_t idx;
   unsigned long result = std::stoul(str, &idx);
-  if (idx != str.size()) throw std::invalid_argument{"invalid str: " + str};
+  if (idx != str.size()) throw std::invalid_argument{"invalid str"};
   if (result > std::numeric_limits<unsigned>::max())
     throw std::out_of_range{"overflow"};
   return result;
 }
 
-unsigned parseNaturalNum(const std::string& str) {
+unsigned parsePositiveNum(const std::string& str) {
   unsigned result = stoui(str);
   if (result == 0) throw std::out_of_range{"number must be greater than 0"};
   return result;
 }
 
-std::vector<std::string_view> split(std::string_view str, char delim) {
-  std::vector<std::string_view> result;
-  auto left = str.begin();
-  for (auto it = left, end = str.end(); it != end; ++it) {
-    if (*it == delim) {
-      result.emplace_back(&*left, it - left);
-      left = it + 1;
-    }
-  }
-  if (left != str.end()) result.emplace_back(&*left, str.end() - left);
-  return result;
-}
-
-event::time_point parseTime(const std::string_view str) {
-  std::tm t;
+event::time_point parseTime(std::string_view str) {
+  std::tm t{};
   std::istringstream in{std::string{str}};
-
   in >> std::get_time(&t, "%H:%M");
   if (in.fail()) throw std::invalid_argument{"invalid time format"};
   return event::time_point{std::chrono::hours{t.tm_hour} +
                            std::chrono::minutes{t.tm_min}};
 };
 
-event::Id parseEventId(const std::string_view str) {
+event::Id parseEventId(std::string_view str) {
   if (str == "1") return event::Id::Come;
   if (str == "2") return event::Id::Seat;
   if (str == "3") return event::Id::Wait;
@@ -57,10 +46,17 @@ event::Id parseEventId(const std::string_view str) {
   throw std::runtime_error{"unknown event id"};
 }
 
-bool validateClientName(const std::string_view str) {
-  return std::all_of(str.begin(), str.end(), [](unsigned char c) {
+bool validateClientName(std::string_view str) {
+  return std::all_of(str.begin(), str.end(), [](auto c) {
     return std::islower(c) || std::isdigit(c) || c == '_';
   });
+}
+
+auto split(const std::string& line) {
+  return line | std::ranges::views::split(' ') |
+         std::ranges::views::transform([](auto&& rng) {
+           return std::string_view(&*rng.begin(), std::ranges::distance(rng));
+         });
 }
 
 std::pair<ComputerClub, std::vector<std::unique_ptr<event::Base>>> parse(
@@ -73,58 +69,63 @@ std::pair<ComputerClub, std::vector<std::unique_ptr<event::Base>>> parse(
   event::time_point openTime, closeTime;
   std::vector<std::unique_ptr<event::Base>> events;
   std::string line;
+
   try {
     std::getline(istream, line);
-    tableNum = parseNaturalNum(line);
+    tableNum = parsePositiveNum(line);
 
     std::getline(istream, line);
-    std::vector strs = split(line, ' ');
+    auto strs = split(line);
+    auto itStr = std::ranges::begin(strs);
+    openTime = parseTime(*itStr);
+    closeTime = parseTime(*++itStr);
 
-    if (strs.size() != 2) throw std::runtime_error{"expected two time points"};
-    openTime = parseTime(strs[0]);
-    closeTime = parseTime(strs[1]);
+    if (++itStr != std::ranges::end(strs))
+      throw std::runtime_error{"expected two time points"};
 
     std::getline(istream, line);
-    pricePerHour = parseNaturalNum(line);
+    pricePerHour = parsePositiveNum(line);
 
     while (std::getline(istream, line)) {
-      auto strs = split(line, ' ');
+      strs = split(line);
+      itStr = std::ranges::begin(strs);
 
-      constexpr int minLength = 3;
-      if (strs.size() < minLength)
-        throw std::runtime_error{"invalid event's format"};
+      auto time = parseTime(*itStr);
+      auto eventId = parseEventId(*++itStr);
 
-      auto time = parseTime(strs[0]);
-      auto eventId = parseEventId(strs[1]);
-
-      std::string clientName{strs[2]};
-      if (!validateClientName(strs[2]))
+      std::string clientName{*++itStr};
+      if (!validateClientName(clientName))
         throw std::runtime_error{"invalid client name"};
 
       switch (eventId) {
         case event::Id::Come: {
           events.push_back(
-              std::move(std::make_unique<event::Come>(time, clientName)));
+              std::make_unique<event::Come>(time, std::move(clientName)));
           break;
         }
         case event::Id::Seat: {
-          unsigned tableNum = parseNaturalNum(std::string{strs[3]});
-          events.push_back(
-              std::make_unique<event::Seat>(time, clientName, tableNum));
+          unsigned tableNum = parsePositiveNum(std::string{*++itStr});
+          events.push_back(std::make_unique<event::Seat>(
+              time, std::move(clientName), tableNum));
           break;
         }
         case event::Id::Wait: {
-          events.push_back(std::make_unique<event::Wait>(time, clientName));
+          events.push_back(
+              std::make_unique<event::Wait>(time, std::move(clientName)));
           break;
         }
         case event::Id::Left: {
-          events.push_back(std::make_unique<event::Left>(time, clientName));
+          events.push_back(
+              std::make_unique<event::Left>(time, std::move(clientName)));
           break;
         }
       }
+
+      if (++itStr != std::ranges::end(strs))
+        throw std::runtime_error{"wrong line"};
     }
 
-  } catch (...) {
+  } catch (const std::exception&) {
     throw std::runtime_error{line};
   }
 
